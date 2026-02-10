c1a02258d648ff0e2ec285cf20cee5a7c4458b09
Fernando <dwk262@gmail.com>
2026-02-10 10:33:09 +0100

    Initial Commit

---
+++ Es de dia o de noche/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Es de dia o de noche
+//
+//  Created by Equipo 9 on 10/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Es de dia o de noche/Es_de_dia_o_de_nocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  Es_de_dia_o_de_nocheApp.swift
+//  Es de dia o de noche
+//
+//  Created by Equipo 9 on 10/2/26.
+//
+
+import SwiftUI
+
+@main
+struct Es_de_dia_o_de_nocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
